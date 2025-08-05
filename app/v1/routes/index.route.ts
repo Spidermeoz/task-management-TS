34959f9e208046818f78a1876a8d5db1b8320b74
SpiderMeow <thoconditonglao123@gmail.com>
2025-08-05 18:11:15 +0700

    Chuyển các routes thành private

--- app/v1/routes/index.route.ts
+++ app/v1/routes/index.route.ts
@@ -3,12 +3,12 @@ import { Express } from "express";
 import { taskRoutes } from "./task.route";
 import { userRoutes } from "./user.route";
 
-// const authMiddleware = require("../middlewares/auth.middleware");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const mainV1Routes = (app: Express): void => {
   const version = "/api/v1";
 
-  app.use(version + "/tasks", taskRoutes);
+  app.use(version + "/tasks", authMiddleware.requireAuth, taskRoutes);
 
   app.use(version + "/users", userRoutes);
 };
