3bcd1a032fe8e78f64d83167d3dbd568f3d26023
SpiderMeow <thoconditonglao123@gmail.com>
2025-08-05 15:34:45 +0700

    Bộ lọc theo trạng thái

--- app/v1/controllers/task.controller.ts
+++ app/v1/controllers/task.controller.ts
@@ -1,21 +1,31 @@
-import {Request, Response} from "express"
+import { Request, Response } from "express";
 import Task from "../models/tasks.models";
 
 export const index = async (req: Request, res: Response) => {
-  const tasks = await Task.find({
+  interface Find {
+    deleted: boolean,
+    status?: string
+  }
+  const find: Find = {
     deleted: false,
-  });
+  };
+
+  if (req.query.status) {
+    find.status = req.query.status.toString();
+  }
+
+  const tasks = await Task.find(find);
 
   res.json(tasks);
-}
+};
 
 export const detail = async (req: Request, res: Response) => {
   const id = req.params.id;
-  
+
   const tasks = await Task.find({
     _id: id,
     deleted: false,
   });
 
   res.json(tasks);
-}
+};
